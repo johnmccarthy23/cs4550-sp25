464c74be4f776bf0736b29d83f1db39afbb55745
John McCarthy <John McCarthy>
2025-02-19 13:05:59 -0500

    a3 JS

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -1,4 +1,3 @@
-import { ListGroup } from "react-bootstrap";
 import ModulesControls from "./ModulesControls";
 import { BsGripVertical } from "react-icons/bs";
 import ModuleControlButtons from "./ModuleControlButtons";
