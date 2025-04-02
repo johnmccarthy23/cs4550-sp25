5f9ce3d7d013637e1bb53d160fd007c3a249dcfd
John McCarthy <John McCarthy>
2025-04-02 14:42:28 -0400

    fix a5

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,4 +1,3 @@
-import { useSelector } from "react-redux";
 import { FormControl } from "react-bootstrap";
 import { Col, Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -9,7 +8,6 @@ export default function Dashboard(
     addNewCourse: () => void; deleteCourse: (course: any) => void;
     updateCourse: () => void; })
    {
-    const { currentUser } = useSelector((state: any) => state.accountReducer);
     return (
     <div className="p-4" id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -34,7 +34,7 @@ export default function Kambaz() {
     setCourses([...courses, { ...course, newCourse }]);
   };
   const deleteCourse = async (courseId: any) => {
-    const status = await courseClient.deleteCourse(courseId);
+    await courseClient.deleteCourse(courseId);
     setCourses(courses.filter((course) => course._id !== courseId));
   };
   const updateCourse = async () => {
